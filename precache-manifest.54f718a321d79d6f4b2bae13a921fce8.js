ef6dd9d49cd251bbb14b92399e3ff7e58f4429db
snyggme <rcbabahin@gmail.com>
2018-10-05 22:34:11 +0300

    Updates

---
+++ precache-manifest.54f718a321d79d6f4b2bae13a921fce8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3399999d14c61ad309c6",
+    "url": "/drum-machine/static/js/runtime~main.3399999d.js"
+  },
+  {
+    "revision": "62cb1802c5e891e211ce",
+    "url": "/drum-machine/static/js/main.62cb1802.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/drum-machine/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "62cb1802c5e891e211ce",
+    "url": "/drum-machine/static/css/main.7929eb6c.chunk.css"
+  },
+  {
+    "revision": "8463a5d06e07c22c9a16a9ec788eddca",
+    "url": "/drum-machine/index.html"
+  }
+];
